dac9ced3537ecda2126b1f98e63ec9cb620e7923
FlowerAndMoon <9394687+flowerandmoon@user.noreply.gitee.com>
2021-07-30 18:54:10 +0800

    用户登录-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LoginIndex from '@/views/login'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginIndex
+  }
+]
 
 const router = new VueRouter({
   routes
